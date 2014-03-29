758dff5db3e1696d6e635adc1981468105ee1e05
catatsuy <catatsuy@catatsuy.org>
2014-03-29 15:19:18 +0900

    とりあえず画像を投稿できるように

--- main.rb
+++ main.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'sqlite3'
+require "securerandom"
 
 db = SQLite3::Database.new "db/test.db"
 db.results_as_hash = true
@@ -10,8 +11,32 @@ get '/' do
 end
 
 post '/' do
+  file_name = nil
+
+  if params["file"]
+    # Content Type は信用ならないが，教育用のために妥協
+    image_reg = /\Aimage\/(?<image_type>jpeg|(?:x-)?png)\Z/
+    match = image_reg.match params["file"][:type]
+    unless match
+      return "error"
+    end
+
+    # Content Type から拡張子を決定して適当な名前で保存
+    ext = nil
+    if match[:image_type].include? "jpeg"
+      ext = "jpg"
+    elsif match[:image_type].include? "png"
+      ext = "png"
+    end
+    file_name = "#{SecureRandom.hex}.#{ext}"
+    save_path = "./public/images/#{file_name}"
+    File.open(save_path, 'wb') do |f|
+      f.write params[:file][:tempfile].read
+    end
+  end
+
   stmt = db.prepare("INSERT INTO posts (text, img_file_name) VALUES (?, ?)")
-  stmt.bind_params(params["ex-text"], "koko.jpg")
+  stmt.bind_params(params["ex-text"], file_name)
   stmt.execute
   redirect '/'
 end

6b61b9a73bfc5c6a3b8492bfcadc1b938b38d6de
catatsuy <catatsuy@catatsuy.org>
2014-03-29 14:26:24 +0900

    適当にテキストだけ投稿できるように

--- main.rb
+++ main.rb
@@ -1,9 +1,17 @@
 require 'sinatra'
+require 'sqlite3'
+
+db = SQLite3::Database.new "db/test.db"
+db.results_as_hash = true
 
 get '/' do
-  "Hello world"
+  posts = db.execute("SELECT id, text, img_file_name FROM posts ORDER BY ID DESC LIMIT 10")
+  erb :example, locals: { posts: posts }
 end
 
-get '/example' do
-  erb :example
+post '/' do
+  stmt = db.prepare("INSERT INTO posts (text, img_file_name) VALUES (?, ?)")
+  stmt.bind_params(params["ex-text"], "koko.jpg")
+  stmt.execute
+  redirect '/'
 end
